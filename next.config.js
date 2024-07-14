bf77e7426b7b87ed0737b85ddc7a48e0e96e62ea
notsayali <karwadesayli@gmail.com>
2024-07-14 19:31:29 +0530

    .

--- next.config.js
+++ next.config.js
@@ -3,13 +3,26 @@ const nextConfig = {
     output: 'export',
     images: {
        unoptimized: true,
-     },
-     eslint: {
+    },
+    eslint: {
         ignoreDuringBuilds: true,
     },
     typescript: {
         ignoreBuildErrors: true
-    }
+    },
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+            '/': { page: '/' },
+            '/about': { page: '/about' },
+            '/visionmission': { page: '/visionmission' },
+            '/brand': { page: '/brand' },
+            '/blog': { page: '/blog' },
+            '/contact': { page: '/contact' },
+        }
+    },
 }
 
 module.exports = nextConfig
